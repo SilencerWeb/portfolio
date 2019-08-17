8b696dae905e03d22d99f8b88973896fee841980
Maksim Gorodov <silencer.web@gmail.com>
2019-08-17 18:44:51 +0500

    Add some changes

--- src/scripts/modules/projects-list.js
+++ src/scripts/modules/projects-list.js
@@ -7,6 +7,7 @@ onDOMReady(() => {
     const counterCurrent = document.querySelector('.counter__current');
 
     new FullPage('.project-list', {
+      autoScrolling: window.innerHeight > 720,
       onLeave: (origin, destination) => {
         let nextSlideNumber = destination.index + 1;
         if (nextSlideNumber < 10) nextSlideNumber = `0${nextSlideNumber}`;
