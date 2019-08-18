25e1303101dce04568bb947d273a25350a6d527c
Maksim Gorodov <silencer.web@gmail.com>
2019-08-18 20:00:58 +0500

    Replace images to videos in some projects

--- src/scripts/modules/projects-list.js
+++ src/scripts/modules/projects-list.js
@@ -8,10 +8,33 @@ onDOMReady(() => {
   new FullPage('.project-list', {
     autoScrolling: window.innerHeight > 720,
     onLeave: (origin, destination) => {
+      const currentProjectVideo = origin.item.querySelector('video.project__media');
+      const nextProjectVideo = destination.item.querySelector('video.project__media');
+
+      if (currentProjectVideo) {
+        setTimeout(() => {
+          currentProjectVideo.pause();
+          currentProjectVideo.currentTime = 0;
+        }, 500);
+      }
+
+      if (nextProjectVideo) {
+        setTimeout(() => {
+          nextProjectVideo.play();
+        }, 500);
+      }
+
       let nextSlideNumber = destination.index + 1;
       if (nextSlideNumber < 10) nextSlideNumber = `0${nextSlideNumber}`;
 
       counterCurrent.innerHTML = nextSlideNumber;
+    },
+    afterLoad: (origin) => {
+      const projectVideo = origin.item.querySelector('video.project__media');
+
+      if (projectVideo) {
+        projectVideo.play();
+      }
     },
   });
 });
